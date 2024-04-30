9ad9634b84ea63bf3be394ed9e7dbef82599f5fb
Aniket Prasad <aniketprasad246@gmail.com>
2024-05-01 05:16:23 +0530

    Added new page Our Vision

---
+++ vision.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Vision</title>
+    <link rel="icon" type="image/x-icon" href="img/logo.png">
+    <!-- custom css file link -->
+    <link rel="stylesheet" href="vision.css">
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+</head>
+
+<body>
+    <!-- header section starts -->
+    <header class="header">
+        <a href="index.php" class="logo">
+            <img src="img/logo.png" alt="">
+        </a>
+
+        <div>
+            <ul id="navbar">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#vision">Our Vision</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </header>
+    <!-- header section ends -->
+
+    <!-- vision section starts -->
+
+    <section class="vision" id="vision">
+
+        <h1 class="heading"><span>Our</span> Vision </h1>
+
+        <div class="row">
+            <div class="image">
+                <img src="img/life2.jpg" alt="">
+            </div>
+
+            <div class="content">
+                <p>Our vision for Organ Donation is grounded in the belief that every individual should
+                    have the opportunity to make a lasting impact through the selfless act of donation,
+                    leaving behind a legacy of life and hope. We aspire to create a world where awareness
+                    and understanding about organ donation are not only prevalent but ingrained in the
+                    societal consciousness, cultivating a culture characterized by empathy and altruism.</p>
+
+                <p>Education serves as a cornerstone of our approach, empowering individuals with the knowledge
+                    needed to make informed decisions about organ donation. Through advocacy efforts, we seek to
+                    dismantle misconceptions and barriers surrounding donation, fostering an environment where
+                    altruistic giving is embraced and encouraged.</p>
+
+            </div>
+        </div>
+    </section>
+
+    <section class="vis" id="vis">
+        <div class="cont">
+            <p>Central to our vision is the establishment of accessible pathways to donation, ensuring that
+                those in need of life-saving organs have equitable opportunities to receive them. By bridging
+                the gap between potential donors and recipients, we strive to mitigate disparities in access to
+                transplantation services, ultimately saving lives and improving health outcomes for countless
+                individuals.</p>
+
+            <p>In realizing our vision, we envision a future where organ donation is not merely tolerated but
+                celebrated as a profound act of generosity and compassion. Each donation represents a beacon of
+                hope, transcending societal boundaries and affirming the interconnectedness of humanity. Through
+                the gift of organ donation, donors and their families leave an enduring legacy of kindness and
+                compassion, touching the lives of recipients and their loved ones in profound ways.</p>
+            <h1>Terms and Conditions:</h1>
+            <p>1. Organ donation is voluntary and without coercion.</p>
+            <p>2. Donors must be of legal age or have parental consent if minors.</p>
+            <p>3. Medical suitability for donation will be determined by qualified healthcare professionals.</p>
+            <p>4. Donors have the right to withdraw consent at any stage of the donation process.</p>
+            <p>5. Recipients will be prioritized based on medical urgency and compatibility.</p>
+            <p>6. The allocation of organs will adhere to established ethical and medical guidelines.</p>
+            <p>7. Donor anonymity will be respected unless otherwise specified by the donor.</p>
+            <p>8. The confidentiality of donor and recipient information will be maintained in accordance
+                with applicable laws and regulations.</p>
+            <p>9. Organ donation does not involve any financial compensation for donors.</p>
+            <p>10. The organization reserves the right to modify these terms and conditions as necessary to
+                ensure compliance with legal and ethical standards.</p>
+        </div>
+
+    </section>
+
+    <!-- vision section ends -->
+
+    <!-- Contact section starts -->
+
+    <section class="contact" id="contact">
+
+        <h1 class="heading"> <span>contact</span> us </h1>
+
+        <div class="row">
+
+            <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d60274.56241474419!2d72.
+80450439318383!3d19.231850885624077!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
+1!3m3!1m2!1s0x3be7b128b333e163%3A0x985640540577af7e!2sBorivali%2C%20Mumbai%2C%
+20Maharashtra!5e0!3m2!1sen!2sin!4v1705246986902!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+            <form action="">
+                <h3>get in touch</h3>
+                <div class="inputBox">
+                    <span class="fas fa-user"></span>
+                    <input type="text" placeholder="First Name">
+
+                    <span class="fas fa-user"></span>
+                    <input type="text" placeholder="Last Name">
+                </div>
+                <div class="inputBox">
+                    <span class="fas fa-envelope"></span>
+                    <input type="email" placeholder="Email Address">
+                </div>
+                <div class="inputBox">
+                    <span class="fas fa-phone"></span>
+                    <input type="number" placeholder="Phone Number">
+                </div>
+                <input type="submit" value="contact now" class="btn">
+            </form>
+        </div>
+
+    </section>
+
+    <!-- Contact section ends -->
+
+    <!-- newsletter section starts -->
+
+    <section id="newsletter" class="section-p1 section-m1">
+        <div class="newstext">
+            <h4>Sign Up For Newsletters</h4>
+            <p>Stay informed about our latest organ donation updates and <span>special opportunities to save
+                    lives.</span></p>
+        </div>
+        <div class="form">
+            <input type="text" placeholder="Your email address">
+            <button class="normal">Sign Up</button>
+        </div>
+    </section>
+
+    <!-- newsletter section ends -->
+
+    <!-- footer section starts -->
+
+    <footer class="section-p1">
+        <div class="col">
+            <img class="logo" src="img/logo.png" alt="">
+            <h4>Contact</h4>
+            <p><strong>Address: </strong> 563 Borivali, Street 36, Mumbai, Maharashtra</p>
+            <p><strong>Phone :</strong>(+91) 22555 33555/ (+91) 23355 23355</p>
+            <p><strong>Hours :</strong>10:00 - 18:00, Mon - Sat</p>
+            <div class="follow">
+                <h4>Follow us</h4>
+                <div class="icon">
+                    <i class="fab fa-facebook-f"></i>
+                    <i class="fab fa-instagram"></i>
+                    <i class="fab fa-x"></i>
+                    <i class="fab fa-linkedin"></i>
+                    <i class="fab fa-youtube"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <h4>About</h4>
+            <a href="index.php">Home</a>
+            <a href="#vision">Our Vision</a>
+            <a href="#donate-organ">Donate Organ</a>
+            <a href="#blogs">Our Blogs</a>
+            <a href="#faqs">FAQ's</a>
+            <a href="#contact">Contact Us</a>
+        </div>
+        <div class="col">
+            <h4>My Account</h4>
+            <a href="login.php">Sign in</a>
+            <a href="#">Help</a>
+        </div>
+        <div class="col install">
+            <h4>Install App</h4>
+            <p>From App Store or Google Play</p>
+            <div class="row">
+                <img src="img/pay/app.jpg" alt="">
+                <img src="img/pay/play.jpg" alt="">
+            </div>
+        </div>
+
+
+        <div class="copyright">
+            <p>© 2024. Life Share</p>
+        </div>
+
+    </footer>
+
+    <!-- footer section ends -->
+</body>
